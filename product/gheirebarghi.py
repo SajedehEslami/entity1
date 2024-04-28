b6a27f6edb85c5f380f8e53357b2b13b4386c4f7
Sajedeh Eslami <168335685+SajedehEslami@users.noreply.github.com>
2024-04-28 13:29:32 +0330

    Delete product directory

--- product/gheirebarghi.py
+++
@@ -1,8 +0,0 @@
-from product import Kala
-
-class NoElectrical(Kala):
-
-    def __init__(self,vazn,gheimat,shomareserial,mahalemasraf):
-        super().__init__(vazn,gheimat,shomareserial)
-        self.mahalemasraf =mahalemasraf
-
