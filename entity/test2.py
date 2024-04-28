ac59ba629f788d869766db842f8edbcf962f7121
Sajedeh Eslami <168335685+SajedehEslami@users.noreply.github.com>
2024-04-28 13:30:58 +0330

    Delete entity directory

--- entity/test2.py
+++
@@ -1,6 +0,0 @@
-from entity.accountants import Accountants
-
-accountants1 = Accountants("ali","bahrami","111-11-1111","12","13")
-accountants1.access_level="12"
-print(accountants1.__dict__)
-
--- entity/test4.py
+++
@@ -1,6 +0,0 @@
-from entity.archive import Archive
-
-archive1 = Archive("ali","bahrami","111-11-1111","12","13","3")
-archive1.floor="12"
-print(archive1.__dict__)
-
